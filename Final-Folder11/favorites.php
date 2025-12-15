d42f0fd0cd73ea35a7cf90c51ce330d06c48cae7
angelovskid1 <angelovskid1@montclair.edu>
2025-12-15 15:47:43 -0500

    Change

--- Final-Folder11/favorites.php
+++ Final-Folder11/favorites.php
@@ -13,8 +13,8 @@ $isGuest = !$isLoggedIn && (isset($_GET['guest']) || ($_SESSION['guest'] ?? fals
 <body>
 <?php include 'nav.php'; ?>
 
-<header class="hero">
-  <div class="hero-content">
+<header class="top">
+  <div class="topstuff">
     <h1>Favorite Recipes</h1>
     <p>All your saved recipes in one place.</p>
 
